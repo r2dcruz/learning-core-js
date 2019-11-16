7c3aaaf18d78c8871ee4508141c14ffae5135ebd
Romaine De La Cruz <r2dcruz@gmail.com>
2019-11-15 22:57:19 -0800

    some js exercise

---
+++ basic.js
@@ -0,0 +1,52 @@
+//Currently this is where all the basic js will go
+//***********************************
+
+//Numbers
+function addTwo(num) {
+  // return the input number plus 2
+  return num + 2;
+}
+
+//***********************************
+
+//strings
+function addTwo(num) {
+  // return the input number plus 2
+  return num + 2;
+}
+
+//***********************************
+
+//arrays 1
+function returnArray(array) {
+  // return the array
+  return array;
+}
+
+//***********************************
+
+//arrays 2
+function returnAnElement(array, index) {
+  // returns array element at given index
+  return array[index];
+}
+
+//***********************************
+
+//arrays 3
+function createAndReturnAnArray() {
+  // create an array
+  var array = [];
+  // return the created array
+  return array;
+}
+
+//***********************************
+
+//objects 1
+function returnObject(object) {
+  // return the object
+  return object;
+}
+
+//***********************************
