0f0c7a1a01a936ef8cab1316378808edb5e857d5
Romaine De La Cruz <r2dcruz@gmail.com>
2019-11-16 22:18:54 -0800

    more basic

--- basic.js
+++ basic.js
@@ -1,7 +1,7 @@
 //Currently this is where all the basic js will go
 //***********************************
 
-//Numbers
+//1) Numbers
 function addTwo(num) {
   // return the input number plus 2
   return num + 2;
@@ -9,7 +9,7 @@ function addTwo(num) {
 
 //***********************************
 
-//strings
+//2) strings
 function addTwo(num) {
   // return the input number plus 2
   return num + 2;
@@ -17,7 +17,7 @@ function addTwo(num) {
 
 //***********************************
 
-//arrays 1
+//3) arrays 1
 function returnArray(array) {
   // return the array
   return array;
@@ -25,7 +25,7 @@ function returnArray(array) {
 
 //***********************************
 
-//arrays 2
+//4) arrays 2
 function returnAnElement(array, index) {
   // returns array element at given index
   return array[index];
@@ -33,7 +33,7 @@ function returnAnElement(array, index) {
 
 //***********************************
 
-//arrays 3
+//5) arrays 3
 function createAndReturnAnArray() {
   // create an array
   var array = [];
@@ -43,10 +43,32 @@ function createAndReturnAnArray() {
 
 //***********************************
 
-//objects 1
+//6) objects 1
 function returnObject(object) {
   // return the object
   return object;
 }
 
 //***********************************
+
+//7) objects 2
+
+function returnAValue(obj, key) {
+  // returns value of inputted object's property located at key
+  return obj[key];
+}
+
+//***********************************
+
+//8) objects 3
+
+function createAndReturnAnObject() {
+  // create an object
+  var obj = {};
+  // return the created object
+  return obj;
+}
+
+//***********************************
+
+//9) objects 4
