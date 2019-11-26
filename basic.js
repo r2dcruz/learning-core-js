015133789151a8f92c2a37d05d05ce85d7fc6b21
Romaine De La Cruz <r2dcruz@gmail.com>
2019-11-25 22:12:23 -0800

    +5 exercise plus 'build a tribute page' on FCC

--- basic.js
+++ basic.js
@@ -683,6 +683,82 @@ console.log('should log [7, 8 , 9, 10]:', resultArray1);
 var resultArray2 = addAnElementInGeneral(['q', 'r', 't'], 2, 's');
 console.log('should log ["q", "r", "s", "t"]:', resultArray2);
 
+//***********************************
+//57) Using .splice() to remove an element
+
+function removeAnElementInGeneral(array, index) {
+  array.splice(index, 1);
+  return array;
+}
+
+var resultArray1 = removeAnElementInGeneral([8, 9, 10, 'bad entry', 11], 3);
+console.log('should log [8, 9, 10, 11]:', resultArray1);
+
+var resultArray2 = removeAnElementInGeneral(['one', 'two', 452, 'three'], 2);
+console.log('should log ["one", "two", "three"]:', resultArray2);
+
+//***********************************
+//58) Using .splice() to remove and add element
+
+function applySplice(array, index, item1, item2) {
+  array.splice(index, 2, item1, item2);
+  return array;
+}
+
+var resultArray1 = applySplice([8, 9, 10, 'bad entry1', 'bad entry2', 13], 3, 11, 12);
+console.log('should log [8, 9, 10, 11, 12, 13]:', resultArray1);
+
+var resultArray2 = applySplice(['one', 'two', 452, 672, 'five'], 2, 'three', 'four');
+console.log('should log ["one", "two", "three", "four", "five"]:', resultArray2);
+
+//***********************************
+//59) Using Array.isArray()
+
+function isAnArray(input) {
+  var result = Array.isArray(input);
+  return result;
+}
+
+var resultBoolean1 = isAnArray([1, 2, 3]);
+console.log('should log true:', resultBoolean1);
+
+var resultBoolean2 = isAnArray({name: 'Tom', friend: 'Jerry'});
+console.log('should log false:', resultBoolean2);
+
+//***********************************
+//60) Using .slice() in general
+
+function applySlice(array, start, end) {
+  var sliceOfArray = array.slice(start, end);
+  return sliceOfArray;
+}
+
+var array1 = ['Q', 'u', 'e', 'u', 'e'];
+var resultOfSlice1 = applySlice(array1, 1, 4);
+console.log('should log ["u", "e", "u"]:', resultOfSlice1);
+
+var array2 = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+var resultOfSlice2 = applySlice(array2, 2, 8);
+console.log('should log [2, 3, 4, 5, 6, 7]:', resultOfSlice2);
+
+//***********************************
+//61) Using .slice() to make a copy
+function makeACopy(array) {
+  var copy = array.slice();
+  return copy;
+}
+
+var array1 = ['Q', 'u', 'e', 'u', 'e'];
+var resultCopy1 = makeACopy(array1);
+console.log('should log ["Q", "u", "e", "u", "e"]:', resultCopy1);
+
+var array2 = [0, 1, 2, 3, 4];
+var resultCopy2 = makeACopy(array2);
+console.log('should log [0, 1, 2, 3, 4]:', resultCopy2);
+
+
+
+
 
 
 
