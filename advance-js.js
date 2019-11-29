14010055f9cd3941bd4a2f7baa39904247dcb477
Romaine De La Cruz <r2dcruz@gmail.com>
2019-11-28 22:31:20 -0800

    add + exercise and a couple of fcc exercise

--- advance-js.js
+++ advance-js.js
@@ -1,6 +1,48 @@
-//******************************************************
+//***********************************
 //1) isOldEnoughToDrink
 
 function isOldEnoughToDrink(age) {
   return age >= 21;
 }
+
+//***********************************
+//2) isOldEnoughToDrive
+
+function isOldEnoughToDrive(age) {
+  // your code here
+  if (age >= 16) {
+      return true;
+  } else {
+      return false;
+  }
+}
+
+var output = isOldEnoughToDrive(22);
+console.log(output); // --> true
+
+//***********************************
+//3) isOldEnoughToVote
+
+function isOldEnoughToVote(age) {
+  // your code here
+  if (age >= 18) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+The legal voting age in the United States is 18.
+var output = isOldEnoughToVote(22);
+console.log(output); // --> true
+
+//***********************************
+//4) isOldEnoughToDrinkAndDrive
+
+function isOldEnoughToDrinkAndDrive(age) {
+  // your code here
+    return false;
+}
+
+var output = isOldEnoughToDrinkAndDrive(22);
+console.log(output); // --> false
