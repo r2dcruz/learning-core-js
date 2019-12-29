acfdd43d864653f5a0478bb13d9567166b745825
Romaine De La Cruz <r2dcruz@gmail.com>
2019-12-28 23:47:45 -0800

    quick ca exercise

--- galvanize-exercise/advance-js.js
+++ galvanize-exercise/advance-js.js
@@ -512,3 +512,16 @@ var output = getNthElement([1, 3, 5], 1);
 console.log(output); // --> 3
 
 //codeacademy exercise
+
+//***********************************
+//2) getFirstElement  
+
+function getFirstElement(array) {
+  var firstElement = array[0];
+  return firstElement;
+}
+
+var output = getFirstElement([1, 2, 3, 4, 5]);
+console.log(output); // --> 1
+
+//Continue codeacademy
