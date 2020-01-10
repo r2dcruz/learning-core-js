3b829ab668ebdea7e58da79e79ca3bc520ed21f7
Romaine De La Cruz <r2dcruz@gmail.com>
2020-01-09 20:39:43 -0800

    reverse()

--- mci-dsAlgorithms/singly-linkedlist.js
+++ mci-dsAlgorithms/singly-linkedlist.js
@@ -72,6 +72,23 @@ class LinkedList {
       leader.next = unwantedNode.next;
       this.length--;
     }
+    reverse() {
+      if (!this.head.next) {
+        return this.head;
+      }
+      let first = this.head;
+      this.tail = this.head;
+      let second = first.next;
+      while (second) {
+        const temp = second.next;
+        second.next = first;
+        first = second;
+        second = temp;
+      }
+      this.head.next = null;
+      this.head = first;
+      return this.printList();
+    }
   }
 
   //calling out function class
