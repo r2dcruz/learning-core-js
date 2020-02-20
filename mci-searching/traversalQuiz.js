f964e466ee7d94848870da02823cfe3f68b0ad7d
Romaine De La Cruz <r2dcruz@gmail.com>
2020-02-19 22:28:45 -0800

    start of search

---
+++ mci-searching/traversalQuiz.js
@@ -0,0 +1,18 @@
+//1) if you know a solution is not far from the root of the tree
+BFS()
+
+//2) if the tree is very deep and solutions are rare,
+BFS (DFS-will-take-long-time)
+
+//3) if the tree is ver wide
+DFS (BFS-will-need-too-much-memory)
+
+//4) if solutions are frequent but located deep in the tree
+DFS()
+
+//5) determing whether a path exists between two nodes
+DFS()
+
+//6) finding the shortest path
+BFS()
+
