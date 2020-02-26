e114a4c3c007aa587575f0bb495d4d9ea4141218
Romaine De La Cruz <r2dcruz@gmail.com>
2020-02-25 17:26:36 -0800

    implementing stacks using linkedlist

--- mci-dsAlgorithms/queues.js
+++ mci-dsAlgorithms/queues.js
@@ -1,6 +1,6 @@
-// Last in, first out
-//
-// lookup o(n)
-// pop o(1)
-// push o(1)
-// peek o(1)
+// first in, first out
+
+// lookup o(n) - searching through the stack specifically
+// enqueue o(1) - add to the front
+// dequeue o(1) - remove the first item of the stack
+// peek o(1) - show the first item of the stack
--- mci-dsAlgorithms/stacks.js
+++ mci-dsAlgorithms/stacks.js
@@ -5,38 +5,56 @@
 // push o(1) - add an item
 // peek o(1) - view the top plate
 
-class LinkedList {
-    constructor(value) {
-        this.head = {
-            value: value,
-            next: null
-        };
-        this.tail = this.head;
-        this.length = 1;
+
+// using LinkedList
+class Node {
+    constructor (value) {
+        this.value = value;
+        this.next = null;
     }
-    append(value) {
-        let newNode = {
-            value: value,
-            next: null
-        };
-        this.tail.next = newNode;
-        this.tail = newNode;
-        this.length++;
-        return this;
-    }
-    prepend(value) {
-        let newNode = {
-            value: value,
-            next: null
-        }
-        newNode.next = this.head;
-        this.head = newNode;
-        this.length++;
-        return this;
-    }
-    insert(index, value) {
-        let leader = null;
-        let holdingPointer = null; 
-    }
-    traverseToIndex
 }
+
+class Stack {
+    constructor() {
+        this.top = null;
+        this.bottom = null;
+        this.length = 0;
+    }
+    peek() {
+      return this.top;
+    }
+    push(value) {
+      const newNode = new Node(value);
+      if (this.length === 0) {
+        this.top = newNode;
+        this.bottom = newNode;
+      } else {
+        const holdingPointer = this.top;
+        this.top = newNode;
+        this.top.next = holdingPointer;
+      }
+      this.length++;
+      return this;
+    }
+    pop() {
+      if (!this.top) {
+        return null;
+      }
+      if (this.top === this.bottom) {
+        this.bottom = null;
+      }
+      this.top = this.top.next;
+      this.length--;
+      return this;
+    }
+}
+
+const stack = new Stack();
+stack.peek();
+stack.push('google');
+stack.push('udemy');
+stack.push('discord');
+stack.peek();
+stack.pop();
+stack.pop();
+stack.pop();
