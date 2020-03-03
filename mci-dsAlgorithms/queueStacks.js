18fc82ae8e31f530d5ab5c0b5f33270c5aeb1c52
Romaine De La Cruz <r2dcruz@gmail.com>
2020-03-02 22:20:32 -0800

    revising practice

--- mci-dsAlgorithms/queueStacks.js
+++ mci-dsAlgorithms/queueStacks.js
@@ -36,30 +36,33 @@ stQueue.enqueue("facebook");
 
 //leet code submission
 //plan to revise and clean
-var MyQueue = function() {
-    this.array = []
-    
-};
-
-MyQueue.prototype.push = function(value) {
-    this.array.push(value)
-};
-
-MyQueue.prototype.pop = function() {
-    let length = this.array.length;
-    if(length > 0){
-        return this.array.shift()
+class StackQueue {
+    construct() {
+        this.first = [];
+        this.last = [];
     }
-    return false
-};
-
-MyQueue.prototype.peek = function() {
-    let length = this.array.length;
-    if(length > 0){
-        return this.array[0]
+    enqueue(value) {
+        const length = this.first.length;
+        for (let i = 0; i < length; i++) {
+            this.last.push(this.first.pop());
+        }
+        this.last.push(value);
+        return this;
     }
-};
+    dequeue() {
+        const length = this.last.length;
+        for (let i = 0; i < length; i++) {
+            this.first.push(this.last.pop());
+        }
+        this.first.pop();
+        return this;
+    }
+    peek(){
+        if (this.last.length > 0) {
+            return this.last[0];
+        }
+        return this.first[this.first.length - 1];
+    }
+}
 
-MyQueue.prototype.empty = function() {
-    return this.array.length === 0
-};
+//cleaned up queuestacks. prefer to use class objects
