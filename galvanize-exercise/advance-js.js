448193f8377fc980291e34023168050a030941d8
Romaine De La Cruz <6153680+r2dcruz@users.noreply.github.com>
2020-03-05 22:57:52 -0800

    start of python learning

--- galvanize-exercise/advance-js.js
+++ galvanize-exercise/advance-js.js
@@ -836,3 +836,27 @@ function getLongestOfThreeWords(word1, word2, word3) {
 
 var output = getLongestOfThreeWords('these', 'three', 'words');
 console.log(output); // --> 'these'
+
+//***********************************
+// 2) findShortestOfThreeWords
+
+function findShortestOfThreeWords(word1, word2, word3) {
+    if (word1.length <= word2.length) {
+        if (word1.length <= word3.length) {
+            return word1;
+        } else {
+            return word3;
+        }
+    } else {
+        if (word2.length <= word3.length) {
+            return word2;
+        } else {
+            return word3;
+        }
+    }
+}
+
+var output = findShortestOfThreeWords('a', 'two', 'three');
+console.log(output); // --> 'a'
+
+//started learning python. not bad.
