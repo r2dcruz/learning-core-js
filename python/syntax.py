c02f542c44fed0add113ed8f0524e8d8c1bb0bba
Romaine De La Cruz <r2dcruz@gmail.com>
2020-03-06 23:18:14 -0800

    starting out python

---
+++ python/syntax.py
@@ -0,0 +1,10 @@
+#starting out my tutorial of python
+
+def reverse_string():
+    array = []
+    string = input("enter a string: ")
+    for x in string:
+        array.append(x)
+    print(array)
+
+reverse_string()
