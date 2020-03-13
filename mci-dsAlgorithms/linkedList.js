cd7e227bcad1b4cbbc0bfd504395b67c3e2e4b95
Romaine De La Cruz <6153680+r2dcruz@users.noreply.github.com>
2020-03-12 21:15:25 -0700

    add reverse function

--- mci-dsAlgorithms/linkedList.js
+++ mci-dsAlgorithms/linkedList.js
@@ -91,6 +91,24 @@ class LinkedList {
     }
     return "Doesn't exist";
   }
+    //reverse linkedList
+    reverse() {
+        if (!this.head.next) {
+        return this.head;
+      }
+      let first = this.head;
+      this.tail = this.head;
+      let second = first.next;
+      while(second) {
+        const temp = second.next;
+        second.next = first;
+        first = second;
+        second = temp;
+      }
+      this.head.next = null;
+      this.head = first;
+      return this.printList();
+    }
 }
 
 let myLinkedList = new LinkedList(10);
