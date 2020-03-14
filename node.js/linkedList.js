e587dc4d78f265c1662fb93ea3332e992b70650a
Romaine De La Cruz <r2dcruz@gmail.com>
2020-03-14 12:26:57 -0700

    ll exercise

--- node.js/linkedList.js
+++ node.js/linkedList.js
@@ -14,10 +14,45 @@ class LinkedList {
         this.tail = this.head;
         this.length = 1;
     }
+    addBack(value) {
+        const newNode = new Node(value);
+        this.tail.next = newNode;
+        this.tail = newNode;
+        this.length++;
+        return this;
+    }
+    addFront(value) {
+        const newNode = new Node(value);
+        const holdingPointer = this.head;
+        this.head = newNode;
+        newNode.next = holdingPointer;
+        this.length++;
+        return this;
+    }
+    insert(index, value) {
+        if (index >= this.length) {
+            return this.addBack(value);
+        }
+        const newNode = new Node(value);
+        const leader = this.traverseToIndex(index-1);
+        const holdingPointer = leader.next;
+        leader.next = newNode;
+        newNode.next = holdingPointer;
+        this.length++;
+        return this;
+    }
+    traverseToIndex(index) {
+        let counter = 0;
+        let currentNode = this.head;
+        while (counter !== index) {
+            currentNode = currentNode.next;
+            counter++;
+        }
+        return currentNode;
+    }
 }
 
 (function test(){
     console.log("test function")
     let ll = new LinkedList(77);
-    console.log(ll);
 })
