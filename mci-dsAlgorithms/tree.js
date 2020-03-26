6c0726ff05be4f1410bbabfd318dfb99a34f870b
Romaine De La Cruz <r2dcruz@gmail.com>
2020-03-25 22:12:27 -0700

    tree practice

--- mci-dsAlgorithms/tree.js
+++ mci-dsAlgorithms/tree.js
@@ -134,3 +134,58 @@ class Node {
   bst.bfs();
   
   // still need to disect this.
+
+  //Another iteration of the tree
+
+  class Node {
+    constructor(value) {
+      this.value = value;
+      this.left = null;
+      this.right = null;
+    }
+  }
+
+  class BST {
+    constructor() {
+      this.root = null;
+    }
+    insert(value) {
+      const newNode = new Node(value);
+      if (this.root === null) {
+        this.root = newNode;
+      } else {
+        let currentNode = this.root;
+        while(true) {
+          if(value < currentNode.value) {
+            if(!currentNode.left) {
+              currentNode.left = newNode;
+              return this;
+            }
+            currentNode = currentNode.left;
+          } else {
+            if (!currrentNode.right) {
+              currentNode.right = newNode;
+              return this;
+            }
+            currentNode = currentNode.right;
+          }
+        }
+      }
+    }
+    lookup(value){
+      if (!this.root) {
+        return false;
+      }
+      let currentNode = this.root;
+      while(currentNode){
+        if(value < currentNode.value){
+          currentNode = currentNode.left;
+        } else if(value > currentNode.value){
+          currentNode = currentNode.right;
+        } else if (currentNode.value === value) {
+          return currentNode;
+        }
+      }
+      return null
+    }
+  }
