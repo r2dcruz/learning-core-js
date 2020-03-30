209a16fc6ab34eef4f6668d18415edbb6fc4da64
Romaine De La Cruz <r2dcruz@gmail.com>
2020-03-29 22:32:46 -0700

    linkedList practice

--- mci-dsAlgorithms/linkedList.js
+++ mci-dsAlgorithms/linkedList.js
@@ -123,3 +123,67 @@ myLinkedList.append("remove");
 myLinkedList.printList();
 
 //linkedList confidence = 95
+
+//practice practice practice
+
+class Node {
+    constructor(value) {
+        value: value;
+        next: null;
+    }
+  }
+
+  class LinkedList {
+      constructor(value) {
+          this.head = {
+              value: value,
+              next: null
+          };
+          this.tail = this.head;
+          this.length = 1;
+      }
+      addBack(value) {
+          const newNode = new Node(value);
+          this.tail.next = newNode;
+          this.tail = newNode;
+          this.length++;
+          return this;
+      }
+      addFront(value) {
+          const newNode = new Node(value);
+          const holdingPointer = this.head;
+          this.head = newNode;
+          newNode.next = holdingPointer;
+          this.length++;
+          return this;
+      }
+      insert(index, value) {
+          const newNode = new Node(value);
+          const leader = this.traverseToIndex(index-1);
+          const holdingPointer = leader.next;
+          leader.next = newNode;
+          newNode.next = holdingPointer
+          this.length++;
+          return this;
+      }
+      traverseToIndex(index) {
+          let counter = 0;
+          let currentNode = this.head;
+          while (counter !== index) {
+              currentNode = currentNode.next;
+              counter++;
+          }
+          return currentNode;
+      }
+      print() {
+          const array = [];
+          let currentNode = this.head;
+          while (currentNode !== null ) {
+              array.push(currentNode.value);
+              currentNode = currentNode.next;
+          }
+          return array;
+      }
+  }
+
+  // 3/29/2020 insert() * print() * remove()
