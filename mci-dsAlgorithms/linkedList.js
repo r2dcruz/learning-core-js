2a38bd91ee9cd30e188f14e1b4aaa822ae8fc192
Romaine De La Cruz <r2dcruz@gmail.com>
2020-04-09 22:07:52 -0700

    small progress

--- mci-dsAlgorithms/linkedList.js
+++ mci-dsAlgorithms/linkedList.js
@@ -81,6 +81,25 @@ class LinkedList {
         }
         return array;
     }
+    print() {
+        const array = [];
+        let currentNode = this.head;
+        while (currentNode !== null) {
+            array.push(currentNode.value) ;
+            currentNode = currentNode.next;
+        }
+        return array;
+    }
+    print() {
+        const array = [];
+        let currentNode = this.head;
+        while (currentNode !== null) {
+            array.push(currentNode.value);
+            currentNode = currentNode.next;
+        }
+        return array;
+    }
+
     search(value) {
     let currentNode = this.head;
     while (currentNode) {
@@ -124,97 +143,6 @@ myLinkedList.printList();
 
 //linkedList confidence = 95
 
-//practice practice practice
-class Node {
-    constructor(value) {
-        this.value = value;
-        this.next = null;
-    }
-}
-
-class LinkedList {
-    constructor(value) {
-        this.head = {
-            value: value,
-            next: null
-        };
-        this.tail = this.head;
-        this.length = 1;
-    }
-    addFront(value) {
-        const newNode = new Node(value);
-        this.tail.next = newNode;
-        this.tail = newNode;
-        this.length++;
-        return this;
-    }
-    addBack(value) {
-        const newNode = new Node(value);
-        const holdingPointer = this.head;
-        this.head = newNode;
-        newNode.next = holdingPointer;
-        this.length++;
-        return this;
-    }
-    insert(index, value) {
-        const newNode = new Node(value);
-        const leader = this.traverseToIndex(index-1);
-        const holdingPointer = leader.next;
-        leader.next = newNode;
-        newNode.next = holdingPointer;
-        this.length++;
-        return this;
-    }
-    traverseToIndex(index) {
-        let counter = 0;
-        let currentNode = this.head;
-        while (counter !== index) {
-            let currentNode = currentNode.next;
-            counter++;
-        }
-        return currentNode;
-    }
-    print() {
-        const array = [];
-        let currentNode = this.head;
-        while (currentNode !==null) {
-            currentNode = currentNode.next;
-        }
-        return array;
-    }
-    print() {
-        const array = [];
-        let currentNode = this.head;
-        while (currentNode !== null) {
-            currentNode = currentNode.next;
-        }
-        return array;
-    }
-    print() {
-        const array = [];
-        let currentNode = this.head;
-        while (currentNode !== null) {
-            currentNode = currentNode.next;
-        }
-        return array;
-    }
-    insert(index) {
-        const newNode = new Node(value);
-        const leader = this.traverseToIndex(index-1);
-        const holdingPointer = leader.next;
-        leader.next = newNode;
-        newNode.next = holdingPointer;
-        this.length++;
-        return this;
-    }
-    traverseToIndex(index) {
-        let counter = 0;
-        let currentNode = this.head;
-        while (counter !== index) {
-            currentNode = currentNode.next;
-            counter++;
-        }
-        return currentNode;
-    }
-}
 // 3/29/2020 insert() * print() * remove()
+
+//cleaning up this practice
