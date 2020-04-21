a2534d5610615469b51c30f95337e6af27dd956f
Romaine De La Cruz <r2dcruz@gmail.com>
2020-04-20 23:47:17 -0700

    420

--- mci-dsAlgorithms/tree.js
+++ mci-dsAlgorithms/tree.js
@@ -451,3 +451,44 @@ class Node {
       return null
     }
   }
+
+  insert(value) {
+    const newNode = new Node(value);
+    if (this.root === null) {
+      this.root = newNode;
+    } else {
+      let currentNode = this.root;
+      while(true) {
+        if(value < currentNode.value) {
+          if(!currentNode.left) {
+            currentNode.left = newNode;
+            return this;
+          }
+          currentNode = currentNode.left;
+        } else {
+          if (!currrentNode.right) {
+            currentNode.right = newNode;
+            return this;
+          }
+          currentNode = currentNode.right;
+        }
+      }
+    }
+  }
+  lookup(value){
+    if (!this.root) {
+      return false;
+    }
+    let currentNode = this.root;
+    while(currentNode){
+      if(value < currentNode.value){
+        currentNode = currentNode.left;
+      } else if(value > currentNode.value){
+        currentNode = currentNode.right;
+      } else if (currentNode.value === value) {
+        return currentNode;
+      }
+    }
+    return null
+  }
+}
