34f463c0e32e2dcf52cd9e44ae2fa66be46419d1
Romaine De La Cruz <6153680+r2dcruz@users.noreply.github.com>
2020-04-29 22:01:07 -0700

    full routine practice

--- mci-dsAlgorithms/linkedList.js
+++ mci-dsAlgorithms/linkedList.js
@@ -325,149 +325,84 @@ ll.addFront(54);
 ll.insert(1,64);
 ll.print();
 
-// you really do forget it if you don't practice everyday
+//need to practice this whole routine
 
 class Node {
-    constructor(value) {
-      this.value = value;
-      this.next = null;
-    }
+  constructor(value) {
+    this.value = value;
+    this.next = null;
   }
-  
-  class LinkedList {
-    constructor(value) {
-      this.head = {
-        value: value,
-        next: null
-      }
-      this.tail = this.head;
-      this.length = 1; 
-    }
-    addBack(value) {
-      const newNode = new Node(value);
-      this.tail.next = newNode;
-      this.tail = newNode;
-      this.length++;
-      return this;
-    }
-    addFront(value) {
-      const newNode = new Node(value);
-      const holdingPointer = this.head;
-      this.head = newNode;
-      newNode.next = holdingPointer;
-      this.length++;
-      return this;
-    }
-    insert(index, value) {
-      const newNode = new Node(value);
-      const leader = this.traverseToIndex(index-1);
-      const holdingPointer = leader.next;
-      leader.next = newNode;
-      newNode.next = holdingPointer;
-      this.length++;
-      return this;
-    }
-    traverseToIndex(index) {
-      let counter = 0;
-      let currentNode = this.head;
-      while (counter !== index) {
-        currentNode = currentNode.next;
-        counter++;
-      }
-      return currentNode;
-    }
-    print() {
-      let array = [];
-      let currentNode = this.head;
-      while (currentNode !== null) {
-        array.push(currentNode.value);
-        currentNode = currentNode.next;
-      }
-      return array;
-    }
-  }
-  
-  let ll = new LinkedList(55);
-  ll.addBack(56);
-  ll.addBack(57);
-  ll.addFront(54);
-  ll.insert(1,64);
-  ll.print();
-  
-  // practice, save, add, commit, push, [repeat]
+}
 
-  class Node {
-    constructor(value) {
-      this.value = value;
-      this.next = null;
-    }
+class LinkedList {
+  constructor(value) {
+    this.head = {
+      value: value,
+      next: null
+    };
+    this.tail = this.head;
+    this.length = 1;
   }
-  
-  class LinkedList {
-    constructor(value) {
-      this.head = {
-        value: value,
-        next: null
-      };
-      this.tail = this.head;
-      this.length = 1;
-    }
-    print() {
-      let array = [];
-      let currentNode = this.head;
-      while (currentNode !== null) {
-        array.push(currentNode.value);
-        currentNode = currentNode.next;
-      }
-      return array;
-    }
-    addBack(value) {
-      const newNode = new Node(value);
-      this.tail.next = newNode;
-      this.tail = newNode;
-      this.length++;
-      return this;
-    }
+  addBack(value) {
+    const newNode = new Node(value);
+    this.tail.next = newNode;
+    this.tail = newNode;
+    this.length++;
+    return this;
   }
-  
-  let ll = new LinkedList(50);
-  ll.addBack(60);
-  //ll.print();
+  print() {
+    let array = [];
+    let currentNode = this.head;
+    while (currentNode !== null) {
+      array.push(currentNode.value);
+      currentNode = currentNode.next;
+    }
+    return array;
+  }
+  addFront(value) {
+    const newNode = new Node(value);
+    const holdingPointer = this.head;
+    this.head = newNode;
+    newNode.next = holdingPointer;
+    this.lenght++;
+    return this;
+  }
+  insert(index, value) {
+    const newNode = new Node(value);
+    const leader = this.traverseToIndex(index-1);
+    const holdingPointer = leader.next;
+    leader.next = newNode;
+    newNode.next = holdingPointer;3
+    this.length++;
+    return this;    
+  }
+  traverseToIndex(index) {
+    let counter = 0;
+    let currentNode = this.head;
+    while (counter !== index) {
+      currentNode = currentNode.next;
+      counter++;
+    }
+    return currentNode;
+  }
+  remove(index) {
+    const leader = this.traverseToIndex(index-1);
+    const unwantedNode = leader.next;
+    leader.next = unwantedNode.next;
+    this.length--;
+    return this;
+  }
+}
 
-  class Node {
-    constructor(value) {
-      this.value = value;
-      this.next = null;
-    }
-  }
-  
-  class LinkedList {
-    constructor(value) {
-      this.head = {
-        value: value,
-        next: null
-      };
-      this.tail = this.head;
-      this.length = 1;
-    }
-    print() {
-      let array = [];
-      let currentNode = this.head;
-      while (currentNode !== null) {
-        array.push(currentNode.value);
-        currentNode = currentNode.next;
-      }
-      return array;
-    }
-    addBack(value) {
-      const newNode = new Node(value);
-      this.tail.next = newNode;
-      this.tail = newNode;
-      this.length++;
-      return this;
-    }
-  }
-  
-  let ll = new LinkedList(50);
-  ll.addBack(60);
-  //ll.print();
+let ll = new LinkedList(66);
+ll.addBack(67);
+ll.addBack(68);
+ll.addBack(69);
+ll.addBack(70);
+ll.addFront(64);
+ll.addFront(63);
+ll.addFront(62);
+ll.addFront(61);
+ll.insert(2, 80);
+ll.remove(2);
+ll.print();
