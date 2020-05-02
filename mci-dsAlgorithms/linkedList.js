d06965ad2e48272a4617354f16ccd8a8f16459c2
Romaine De La Cruz <6153680+r2dcruz@users.noreply.github.com>
2020-05-01 23:05:01 -0700

    30 mins of studying

--- mci-dsAlgorithms/linkedList.js
+++ mci-dsAlgorithms/linkedList.js
@@ -406,3 +406,84 @@ ll.addFront(61);
 ll.insert(2, 80);
 ll.remove(2);
 ll.print();
+
+//my goto practice code
+class Node {
+  constructor(value) {
+    this.value = value;
+    this.next = null;
+  }
+}
+
+class LinkedList {
+  constructor(value) {
+    this.head = {
+      value: value,
+      next: null
+    };
+    this.tail = this.head;
+    this.length = 1;
+  }
+  addBack(value) {
+    const newNode = new Node(value);
+    this.tail.next = newNode;
+    this.tail = newNode;
+    this.length++;
+    return this;
+  }
+  print() {
+    let array = [];
+    let currentNode = this.head;
+    while (currentNode !== null) {
+      array.push(currentNode.value);
+      currentNode = currentNode.next;
+    }
+    return array;
+  }
+  addFront(value) {
+    const newNode = new Node(value);
+    const holdingPointer = this.head;
+    this.head = newNode;
+    newNode.next = holdingPointer;
+    this.lenght++;
+    return this;
+  }
+  insert(index, value) {
+    const newNode = new Node(value);
+    const leader = this.traverseToIndex(index-1);
+    const holdingPointer = leader.next;
+    leader.next = newNode;
+    newNode.next = holdingPointer;3
+    this.length++;
+    return this;    
+  }
+  traverseToIndex(index) {
+    let counter = 0;
+    let currentNode = this.head;
+    while (counter !== index) {
+      currentNode = currentNode.next;
+      counter++;
+    }
+    return currentNode;
+  }
+  remove(index) {
+    const leader = this.traverseToIndex(index-1);
+    const unwantedNode = leader.next;
+    leader.next = unwantedNode.next;
+    this.length--;
+    return this;
+  }
+}
+
+let ll = new LinkedList(66);
+ll.addBack(67);
+ll.addBack(68);
+ll.addBack(69);
+ll.addBack(70);
+ll.addFront(64);
+ll.addFront(63);
+ll.addFront(62);
+ll.addFront(61);
+ll.insert(2, 80);
+ll.remove(2);
+ll.print();
