733733244f4012e032f986e13d5c68ec4abcbab5
Romaine De La Cruz <6153680+r2dcruz@users.noreply.github.com>
2020-05-09 00:51:31 -0700

    freecodecamp reading

--- mci-dsAlgorithms/linkedList.js
+++ mci-dsAlgorithms/linkedList.js
@@ -665,3 +665,65 @@ ll.addFront(54);
 ll.addBack(58);
 ll.insert(1, 56);
 ll.print();
+
+//freecodecamp today
+
+class LinkedList {
+  constructor(value) {
+    this.head = {
+      value: value,
+      next: null
+    };
+    this.tail = this.head;
+    this.length = 1;
+  }
+  addBack(value) {
+    const newNode = new Node(value);
+    this.tail.next = newNode;
+    this.tail = newNode;
+    this.length++;
+    return this;
+  }
+  addFront(value) {
+    const newNode = new Node(value);
+    const holdingPointer = this.head;
+    this.head = newNode;
+    newNode.next = holdingPointer;
+    this.length++;
+    return this;
+  }
+  insert(index, value) {
+    const newNode = new Node(value);
+    const leader = this.traverseToIndex(index-1);
+    const holdingPointer = leader.next;
+    leader.next = newNode;
+    newNode.next = holdingPointer;
+    this.length++;
+    return this;    
+  }
+  traverseToIndex(index) {
+    let counter = 0;
+    let currentNode = this.head;
+    while (counter !== index) {
+      currentNode = currentNode.next;
+      counter++;
+    }
+    return currentNode;
+  }
+  print() {
+    let array = [];
+    let currentNode = this.head;
+    while (currentNode !== null) {
+      array.push(currentNode.value) ;
+      currentNode = currentNode.next;
+    }
+    return array;
+  }
+}
+
+let ll = new LinkedList(55);
+ll.addBack(57);
+ll.addFront(54);
+ll.addBack(58);
+ll.insert(1, 56);
+ll.print();
