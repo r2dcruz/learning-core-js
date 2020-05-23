fafc22b073d34116af22a0c56f2e5ab6d552eac6
Romaine De La Cruz <6153680+r2dcruz@users.noreply.github.com>
2020-05-22 22:49:06 -0700

    another bicycle ride

--- mci-dsAlgorithms/practice.js
+++ mci-dsAlgorithms/practice.js
@@ -302,3 +302,98 @@ ll.insert(1, 41);
 ll.search(53);
 ll.remove(3)
 ll.print();
+
+class LinkedList {
+  constructor(value) {
+    this.head = {
+      value: value,
+      next: null
+    };
+    this.tail = this.head;
+    this.length = 1;
+  }
+  addBack(value) {
+    const newNode = new Node(value);
+    this.tail.next = newNode;
+    this.tail = newNode;
+    this.length++;
+    return this;
+  }
+  addFront(value) {
+    const newNode = new Node(value);
+    const holdingPointer = this.head;
+    this.head = newNode;
+    newNode.next = holdingPointer;
+    this.length++;
+    return this;
+  }
+  print() {
+    let array = [];
+    let currentNode = this.head;
+    while (currentNode != null) {
+      array.push(currentNode.value);
+      currentNode = currentNode.next;
+    }
+    return array;
+  }
+  insert(index, value) {
+    const newNode = new Node(value);
+    const leader = this.traverseToIndex(index-1);
+    const holdingPointer = leader.next;
+    leader.next = newNode;
+    newNode.next = holdingPointer;
+    this.length++;
+    return this;
+    
+  }
+  traverseToIndex(index) {
+    let counter = 0;
+    let currentNode = this.head;
+    while (counter != index) {
+      currentNode = currentNode.next;
+      counter++;
+    }
+    return currentNode;
+  }
+  search(value) {
+    let currentNode = this.head;
+    while (currentNode) {
+      if (currentNode.value === value) {
+        return currentNode;
+      }
+      currentNode = currentNode.next;
+    }
+    return "Doesn't exit";
+  }
+  reverse() {
+    let first = this.head;
+    this.tail = this.head;
+    let second = first.next;
+    while (second) {
+      const temp = second.next;
+      second.next = first;
+      first = second;
+      second = temp;
+    }
+    this.head.next = null;
+    this.head = first;
+    return this.print();
+  }
+  remove(index) {
+    const leader = this.traverseToIndex(index-1);
+    const unwantedNode = leader.next;
+    leader.next = unwantedNode.next;
+    this.length--;
+    return this;
+  }
+}
+
+let ll = new LinkedList(50);
+ll.addBack(51);
+ll.addFront(40);
+ll.addBack(52);
+ll.addBack(53);
+ll.insert(1, 41);
+ll.search(53);
+ll.remove(3)
+ll.print();
