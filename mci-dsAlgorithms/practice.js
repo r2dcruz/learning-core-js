3d96b4f288550b1301203f1b4cb8d4daf5cba95e
Romaine De La Cruz <r2dcruz@gmail.com>
2020-06-02 23:33:01 -0700

    back at it

--- mci-dsAlgorithms/practice.js
+++ mci-dsAlgorithms/practice.js
@@ -493,3 +493,73 @@ ll.insert(1, 41);
 ll.search(53);
 ll.remove(3)
 ll.print();
+
+//refresh practice;
+class Node {
+  constructor(value) {
+    this.value = value;
+    this.next = null;
+  }
+}
+
+class LinkedList {
+  constructor(value) {
+    this.head = {
+      value: value,
+      next: null
+    };
+    this.tail = this.head;
+    this.length = 1;
+  }
+  addBack(value) {
+    const newNode = new Node(value);
+    this.tail.next = newNode;
+    this.tail = newNode;
+    this.length++;
+    return this;
+  }
+  addFront(value) {
+    const newNode = new Node(value);
+    const holdingPointer = this.head;
+    this.head = newNode;
+    newNode.next = holdingPointer;
+    this.length++;
+    return this;
+  }
+  print(){
+    let array = [];
+    let currentNode = this.head;
+    while (currentNode !== null) {
+      array.push(currentNode.value);
+      currentNode = currentNode.next;
+    }
+    return array;
+  }
+  insert(index, value) {
+    const newNode = new Node(value);
+    const currentNode = this.head;
+    const leader = this.traverseToIndex(index-1);
+    const holdingPointer = leader.next;
+    leader.next = newNode;
+    newNode.next = holdingPointer;
+    this.length++;
+    return this;
+    }
+
+  traverseToIndex(index) {
+    let counter =  0;
+    const currentNode  = this.head;
+    while (counter !== index) {
+      currentNode = currentNode.next;
+    }
+    return currentNode;
+  }
+}
+
+let ll = new LinkedList(55);
+ll.addBack(56);
+ll.addFront(50);
+ll.addFront(40);
+ll.addBack(60);
+ll.insert(1, 52);
+ll.print();
