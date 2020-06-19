8850386c83e10cfec5e4115df5f01aea69becb37
Romaine De La Cruz <6153680+r2dcruz@users.noreply.github.com>
2020-06-18 20:57:57 -0700

    practice makes perfect

--- mci-dsAlgorithms/practice.js
+++ mci-dsAlgorithms/practice.js
@@ -617,3 +617,52 @@ class LinkedList {
 
 let ll = new LinkedList(50);
 ll
+
+//doing another scratch practice
+
+class Node {
+  constructor(value) {
+    this.value = value;
+    this.next = null;
+  }
+}
+
+class LinkedList {
+  constructor(value) {
+    this.head = {
+      value: value,
+      next: null
+    }
+    this.tail = this.head;
+    this.length = 1;
+  }
+  addBack(value) {
+    const newNode = new Node(value);
+    this.tail.next = newNode;
+    this.tail = newNode;
+    this.length++;
+    return this;
+  }
+  addFront(value) {
+    const newNode = new Node(value);
+    const holdingPointer = this.head;
+    this.head = newNode;
+    newNode.next = holdingPointer;
+    this.length++;
+    return this;
+  }
+  print() {
+    let array = [];
+    let currentNode = this.head;
+    while (currentNode !== null) {
+      array.push(currentNode.value);
+      currentNode = currentNode.next;
+    }
+    return array;
+  }
+}
+
+let ll = new LinkedList(66);
+ll.addBack(67);
+ll.addFront(60);
+ll.print();
