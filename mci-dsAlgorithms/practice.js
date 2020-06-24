e7d13efbce53d3617172c41ef6e2c7715913ad6c
Romaine De La Cruz <r2dcruz@gmail.com>
2020-06-23 23:58:55 -0700

    another repeat

--- mci-dsAlgorithms/practice.js
+++ mci-dsAlgorithms/practice.js
@@ -672,3 +672,114 @@ class LinkedList {
 
 let ll = new LinkedList(50);
 ll
+
+class Node {
+  constructor(value) {
+    this.value = value;
+    this.next = null;
+  }
+}
+
+class LinkedList {
+  constructor(value) {
+    this.head = {
+      value: value,
+      next: null
+    };
+    this.tail = this.head;
+    this.length = 1;
+  }
+  addBack(value) {
+    const newNode = new Node(value);
+    this.tail.next = newNode;
+    this.tail = newNode;
+    this.length++;
+    return this;
+  }
+  addFront(value) {
+    const newNode = new Node(value);
+    const holdingPointer = this.head;
+    this.head = newNode;
+    newNode.next = holdingPointer
+    this.length++;
+    return this;
+  }
+  insert(index, value) {
+    const newNode = new Node(value);
+    const leader = this.traverseToIndex(index-1);
+    const holdingPointer = leader.next;
+    leader.next = newNode;
+    newNode.next = holdingPointer;
+    this.length++;
+    return this;
+  }
+  traverseToIndex(index) {
+    let counter = 0;
+    let currentNode = this.head;
+    while (counter !== index) {
+      currentNode = currentNode.next;
+      counter++;
+    }
+    return currentNode;
+  }
+}
+
+let ll = new LinkedList(50);
+ll
+
+
+class Node {
+  constructor(value) {
+    this.value = value;
+    this.next = null;
+  }
+}
+
+class LinkedList {
+  constructor(value) {
+    this.head = {
+      value: value,
+      next: null
+    };
+    this.tail = this.head;
+    this.length = 1;
+  }
+  addBack(value) {
+    const newNode = new Node(value);
+    this.tail.next = newNode;
+    this.tail = newNode;
+    this.length++;
+    return this;
+  }
+  addFront(value) {
+    const newNode = new Node(value);
+    const holdingPointer = this.head;
+    this.head = newNode;
+    newNode.next = holdingPointer
+    this.length++;
+    return this;
+  }
+  insert(index, value) {
+    const newNode = new Node(value);
+    const leader = this.traverseToIndex(index-1);
+    const holdingPointer = leader.next;
+    leader.next = newNode;
+    newNode.next = holdingPointer;
+    this.length++;
+    return this;
+  }
+  traverseToIndex(index) {
+    let counter = 0;
+    let currentNode = this.head;
+    while (counter !== index) {
+      currentNode = currentNode.next;
+      counter++;
+    }
+    return currentNode;
+  }
+}
+
+let ll = new LinkedList(50);
+ll
+
+
