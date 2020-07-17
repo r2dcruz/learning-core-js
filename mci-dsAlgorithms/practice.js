71f162f5e3b734e30a057ba87c2b037730a42048
Romaine De La Cruz <6153680+r2dcruz@users.noreply.github.com>
2020-07-16 23:26:52 -0700

    stacks practice

--- mci-dsAlgorithms/practice.js
+++ mci-dsAlgorithms/practice.js
@@ -663,4 +663,45 @@ myLinkedList.append("traverseFunc");
 myLinkedList.append("remove");
 myLinkedList.printList();
 
+class Node {
+    constructor (value) {
+        this.value = value;
+        this.next = null;
+    }
+}
 
+class Stack {
+    constructor() {
+        this.top = null;
+        this.bottom = null;
+        this.length = 0;
+    }
+    peek() {
+      return this.top;
+    }
+    push(value) {
+      const newNode = new Node(value);
+      if (this.length === 0) {
+        this.top = newNode;
+        this.bottom = newNode;
+      } else {
+        const holdingPointer = this.top;
+        this.top = newNode;
+        this.top.next = holdingPointer;
+      }
+      this.length++;
+      return this;
+    }
+    pop() {
+      if (!this.top) {
+        return null;
+      }
+      if (this.top === this.bottom) {
+        this.bottom = null;
+      }
+      const holdingPointer = this.top //temp hold if we still want this value
+      this.top = this.top.next;
+      this.length--;
+      return this;
+    }
+}
