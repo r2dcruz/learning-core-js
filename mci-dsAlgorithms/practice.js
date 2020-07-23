a7d82835e85ff83fbd83340bcfb0013d0fc102ee
Romaine De La Cruz <r2dcruz@gmail.com>
2020-07-22 23:26:11 -0700

    pity commit

--- mci-dsAlgorithms/practice.js
+++ mci-dsAlgorithms/practice.js
@@ -662,3 +662,123 @@ myLinkedList.append("index");
 myLinkedList.append("traverseFunc");
 myLinkedList.append("remove");
 myLinkedList.printList();
+
+class LinkedList {
+  constructor(value) {
+      this.head = {
+          value: value,
+          next: null
+      };
+      this.tail = this.head;
+      this.length = 1;
+  }
+  //add to the end
+  append(value) {
+      const newNode = {
+          value: value,
+          next: null
+      };
+      this.tail.next = newNode;
+      this.tail = newNode;
+      this.length++;
+      return this;
+  }
+  //add to the front
+  prepend(value) {
+      const newNode = {
+          value: value,
+          next: null
+      };
+      newNode.next = this.head;
+      this.head = newNode;
+      this.length++;
+      return this;
+  }
+  //specific add
+  insert(index, value) {
+      if (index >= this.length) {
+          return this.append(value);
+      }
+      const newNode = {
+          value: value,
+          next: null
+      };
+      const leader = this.traverseToIndex(index -1);
+      const holdingPointer = leader.next;
+      leader.next = newNode;
+      newNode.next = holdingPointer;
+      this.length++;
+      return this;
+  }
+  traverseToIndex(index) {
+      //next to check params
+      if (index >= this.length) {
+        return "Check your index";
+      }
+
+      let counter = 0;
+      let currentNode = this.head;
+      while (counter !== index) {
+          currentNode = currentNode.next;
+          counter++;
+      }
+      return currentNode;
+  }
+  remove(index) {
+      //need to check params
+      if (index >= this.length) {
+        return "Check your index";
+      }
+      const leader = this.traverseToIndex(index-1);
+      const unwantedNode = leader.next;
+      leader.next = unwantedNode.next;
+      this.length--;
+      return this;
+  }
+  printList() {
+      const array = [];
+      let currentNode = this.head;
+      while (currentNode !== null) {
+          array.push(currentNode.value);
+          currentNode = currentNode.next;
+      }
+      return array;
+  }
+  print() {
+      const array = [];
+      let currentNode = this.head;
+      while (currentNode !== null) {
+          array.push(currentNode.value) ;
+          currentNode = currentNode.next;
+      }
+      return array;
+  }
+  search(value) {
+  let currentNode = this.head;
+  while (currentNode) {
+    if (currentNode.value === value) {
+      return currentNode;
+    }
+    currentNode = currentNode.next;
+  }
+  return "Doesn't exist";
+}
+  //reverse linkedList
+  reverse() {
+      if (!this.head.next) {
+      return this.head;
+    }
+    let first = this.head;
+    this.tail = this.head;
+    let second = first.next;
+    while(second) {
+      const temp = second.next;
+      second.next = first;
+      first = second;
+      second = temp;
+    }
+    this.head.next = null;
+    this.head = first;
+    return this.printList();
+  }
+}
