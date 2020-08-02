b8fc2039d7b191956b3a6d7d2c0b626925095e75
Romaine De La Cruz <6153680+r2dcruz@users.noreply.github.com>
2020-08-01 21:28:26 -0700

    read up on another tree course

--- mci-dsAlgorithms/tree.js
+++ mci-dsAlgorithms/tree.js
@@ -133,5 +133,50 @@ class Node {
   bst.dfsPostOrder();
   bst.bfs();
   
-  // still need to disect this.
-  // been doing this tree all wrong. need to redo it
+// still need to disect this.
+// been doing this tree all wrong. need to redo it
+
+//pulled from MCI: DS + A course.
+
+class Node {
+  constructor(value){
+    this.left = null;
+    this.right = null;
+    this.value = value;
+  }
+}
+
+class BinarySearchTree {
+  constructor(){
+    this.root = null;
+  }
+  insert(value){
+    const newNode = new Node(value);
+    if (this.root === null) {
+      this.root = newNode;
+    } else {
+      let currentNode = this.root;
+      while(true){
+        if(value < currentNode.value){
+          //Left
+          if(!currentNode.left){
+            currentNode.left = newNode;
+            return this;
+          }
+          currentNode = currentNode.left;
+        } else {
+          //Right
+          if(!currentNode.right){
+            currentNode.right = newNode;
+            return this;
+          } 
+          currentNode = currentNode.right;
+        }
+      }
+    }
+  }
+  lookup(value){
+    //Code here
+  }
+}
+
