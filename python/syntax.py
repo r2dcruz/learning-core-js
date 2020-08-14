bdf8d59cec3d4646843e3db52c40d37b3bcf1a94
Romaine De La Cruz <r2dcruz@gmail.com>
2020-08-13 22:45:24 -0700

    python practice

--- python/syntax.py
+++ python/syntax.py
@@ -23,3 +23,25 @@ if data:
     print("data is not none")
 else:
      print("data is none")
+
+# mini game using while
+
+import random
+
+playerhp = 260
+enemyatkl = 60
+enemyatkh = 80
+
+while playerhp > 30:
+  dmg = random.randrange(enemyatkl, enemyatkh)
+  playerhp = playerhp - dmg
+
+  if playerhp <= 30:
+    playerhp = 30
+
+  print("enemy strikes for ", dmg, " points of damage. Current HP is", playerhp)
+
+  if playerhp == 30:
+    print("you have died. You cannot respawn, as you are dead.")
+    break
+
