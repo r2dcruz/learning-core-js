e83c93aa87dfff6ec029bf1bb777c8e3facb6db0
Romaine De La Cruz <r2dcruz@gmail.com>
2020-08-27 16:35:29 -0700

    still practicing but changing my motivation

--- mci-dsAlgorithms/practice.js
+++ mci-dsAlgorithms/practice.js
@@ -897,4 +897,65 @@ class BST {
   }
 } //insert function 
 
-//leaving my BST practice here, and starting my react courses again. Need to do something different and then i'll back!
+//leaving my BST practice here, and starting my react courses again. Need to do something different and then i'll back!git
+
+
+class Node {
+  constructor(value) {
+      this.value = value;
+      this.next = null;
+  }
+}
+
+class LinkedList {
+  constructor(value) {
+      this.head = {
+          value: value,
+          next: null
+      };
+      this.tail = this.head;
+      this.length = 1;
+  }
+  addBack(value) {
+      const newNode = new Node(value);
+      this.tail.next = newNode;
+      this.tail = newNode;
+      this.length++;
+      return this;
+  }
+  addFront(value) {
+      const newNode = new Node(value);
+      const holdingPointer = this.head;
+      this.head = newNode;
+      newNode.next = holdingPointer;
+      this.length++;
+      return this;
+  }
+  insert(index, value) {
+      const newNode = new Node(value);
+      const leader = this.traverseToIndex(index-1);
+      const holdingPointer = leader.next;
+      leader.next = newNode;
+      newNode.next = holdingPointer;
+      this.length++;
+      return this;
+  }
+  traverseToIndex(index) {
+      let counter = 0;
+      let currrentNode = this.head;
+      while (counter !== index) {
+          currrentNode = currentNode.next;
+          counter++;
+      }
+      return currentNode;
+  }
+  print() {
+      const array = [];
+      let currentNode = this.head;
+      while (currentNode !== null) {
+          array.push(currentNode.value);
+          currentNode = currentNode.next;
+      }
+      return array;
+  }
+}
