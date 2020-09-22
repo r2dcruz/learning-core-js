6a24f23270d76b9400ab890f1084ff06434818f6
Romaine De La Cruz <r2dcruz@gmail.com>
2020-09-22 14:21:14 -0700

    dad commit

--- mci-dsAlgorithms/practice.js
+++ mci-dsAlgorithms/practice.js
@@ -1033,3 +1033,18 @@ void creditList(string name, int age) {
 
 //**************************
 
+#include <iostream>
+using namespace std;
+
+void creditList(string name, int age);
+
+int main() {
+  creditList("Allison", 99);
+}
+
+void creditList(string name, int age) {
+  cout << "my name is " << name << endl;
+  cout << "my age is " << age << endl;
+}
+
+//i'm officially a dad!
