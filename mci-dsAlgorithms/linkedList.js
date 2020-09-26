caa9f7b14be4d583f55ec48299e616e6bd8120fa
Romaine De La Cruz <r2dcruz@gmail.com>
2020-09-25 21:11:09 -0700

    dad commit

--- mci-dsAlgorithms/linkedList.js
+++ mci-dsAlgorithms/linkedList.js
@@ -921,3 +921,69 @@ class LinkedList {
         return array;
     }
 }
+
+//practicing linkedList again. Don't want to forget any of this!
+
+class Node {
+  constructor(value) {
+  this.value = value;
+  this.next = null;
+  }
+}
+
+class LinkedList {
+  constructor(value) {
+    this.head = {
+      value: value,
+      next: null
+    };
+    this.tail = this.head;
+    this.length = 1;
+  }
+  append(value) {
+    const newNode = new Node(value);
+    this.tail.next = newNode;
+    this.tail = newNode;
+    this.length++;
+  }
+  prepend(value) {
+    const newNode = new Node(value);
+    const holdingPointer = this.head;
+    this.head = newNode;
+    newNode.next = holdingPointer;
+    this.length++;
+  }
+  print() {
+    const array = [];
+    let currentNode = this.head;
+    while (currentNode != null) {
+      array.push(currentNode.value);
+      currentNode = currentNode.next;
+    }
+    return array;
+  }
+  insert(index, value) {
+    const newNode = new Node(value);
+    const leader = this.traverseToIndex(index-1);
+    const holdingPointer = leader.next;
+    leader.next = newNode;
+    newNode.next = holdingPointer;
+    this.length++;
+    return this;
+  }
+  traverseToIndex(index) {
+    let counter = 0;
+    let currentNode = this.head;
+    while (counter !== index) {
+      currentNode = currentNode.next;
+      counter++
+    }
+    return currentNode;
+  }
+}
+
+let ll = new LinkedList(55);
+ll.append(12);
+ll.prepend(1);
+ll.insert(1,99);
+ll.print();
