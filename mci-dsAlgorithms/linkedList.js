2e04fb7e85c4654a9e82cbd32052085f00113734
Romaine De La Cruz <r2dcruz@gmail.com>
2020-09-27 21:02:20 -0700

    yeah, i like linkedlist

--- mci-dsAlgorithms/linkedList.js
+++ mci-dsAlgorithms/linkedList.js
@@ -987,3 +987,79 @@ ll.append(12);
 ll.prepend(1);
 ll.insert(1,99);
 ll.print();
+
+// can't stop with linkedlist
+
+class Node {
+  constructor(value) {
+  this.value = value;
+  this.next = null;
+  }
+}
+
+class LinkedList {
+  constructor(value) {
+    this.head = {
+      value: value,
+      next: null
+    };
+    this.tail = this.head;
+    this.length = 1;
+  }
+  append(value) {
+    const newNode = new Node(value);
+    this.tail.next = newNode;
+    this.tail = newNode;
+    this.length++;
+    return this;
+  }
+  prepend(value) {
+    const newNode = new Node(value);
+    const holdingPointer = this.head;
+    this.head = newNode;
+    newNode.next = holdingPointer;
+    this.length++;
+    return this;
+  }
+  print() {
+    const array = [];
+    let currentNode = this.head;
+    while (currentNode != null) {
+      array.push(currentNode.value);
+      currentNode = currentNode.next;
+    }
+    return array;
+  }
+  insert(index, value) {
+    const newNode = new Node(value);
+    const leader = this.traverseToIndex(index-1);
+    const holdingPointer = leader.next;
+    leader.next = newNode;
+    newNode.next = holdingPointer;
+    this.length++;
+    return this;
+  }
+  traverseToIndex(index) {
+    let counter = 0;
+    let currentNode = this.head;
+    while (counter !== index) {
+      currentNode = currentNode.next;
+      counter++
+    }
+    return currentNode;
+  }
+  remove() {
+    const leader = this.traverseToIndex(index-1);
+    const unwantedNode = leader.next;
+    leader.next = unwantedNode.next;
+    this.length--;
+    return this;
+  }
+}
+
+let ll = new LinkedList(55);
+ll.append(12);
+ll.prepend(1);
+ll.insert(1,99);
+//ll.remove();
+ll.print();
