ebe4099caa08a030a681945fcc09311b28558f08
Romaine De La Cruz <r2dcruz@gmail.com>
2020-10-13 18:02:34 -0700

    stack life

--- mci-dsAlgorithms/stacks.js
+++ mci-dsAlgorithms/stacks.js
@@ -86,3 +86,51 @@ class Stack{
   st.pop();
   st.pop();
   st.pop();
+
+
+// another practice for stacks! need to know this in my sleep;
+
+class Node {
+  constructor(value) {
+    this.value = value;
+    this.next = null;
+  }
+}
+
+class Stack {
+  constructor(value) {
+    this.top = null;
+    this.bottom = null;
+    this.length = 0;
+  }
+  push(value) {
+    const newNode = new Node(value);
+    if (this.length === 0) {
+      this.top = newNode;
+      this.bottom = newNode;
+    } else {
+      const holdingPointer = this.top;
+      this.top = newNode;
+      this.top.next = holdingPointer;
+    }
+    this.length++;
+    return this;
+  }
+  pop() {
+    if (!this.top) {
+      return null;
+    }
+    if (this.top === this.bottom) {
+      this.bottom = null;
+    }
+    const holdingPointer = this.top;
+    this.top = this.top.next;
+    this.length--;
+    return this;
+  }
+}
+
+let st = new Stack();
+st.push("google");
+st.push("twitter");
+st.pop();
