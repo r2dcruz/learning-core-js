67e97ac0a7f7b80df655750d13db6edd5ab0c946
Romaine De La Cruz <r2dcruz@gmail.com>
2020-10-13 17:49:05 -0700

    queues, queues

--- mci-dsAlgorithms/queues.js
+++ mci-dsAlgorithms/queues.js
@@ -97,3 +97,51 @@ class Queue {
     return this.first;
   }
 }
+
+//practice queues. really easy, trying not to forget this 
+
+class Node {
+  constructor(value) {
+    this.value = value;
+    this.next = null;
+  }
+}
+
+class Queue {
+  constructor(value) {
+    this.first = null;
+    this.last = null;
+    this.length = 0;
+  }
+  enqueue(value) {
+    const newNode = new Node(value);
+    if (this.length === 0) {
+      this.first = newNode;
+      this.last = newNode;
+    } else {
+      this.last.next = newNode;
+      this.last = newNode;
+    }
+    this.length++;
+    return this;
+  }
+  dequeue() {
+    if (!this.first) {
+      return null;
+    }
+    if (this.first === this.last) {
+      this.last = null;
+    } 
+    this.first = this.first.next;
+    this.length--;
+    return this;
+  }
+  peek() {
+    return this.first;
+  }
+}
+
+let qu = new Queue();
+qu.enqueue("google");
+qu.enqueue("twitter");
+qu.dequeue();
