d0e420530ba130606f2140584ad2c4747165e29b
Romaine De La Cruz <r2dcruz@gmail.com>
2020-10-24 19:50:54 -0700

    hello push

--- mci-dsAlgorithms/queues.js
+++ mci-dsAlgorithms/queues.js
@@ -145,3 +145,48 @@ let qu = new Queue();
 qu.enqueue("google");
 qu.enqueue("twitter");
 qu.dequeue();
+
+// get a rythem going and keep practicing 
+
+class Node {
+  constructor (value) {
+    this.value = value;
+    this.next = null;
+  }
+}
+
+class Queue {
+  constructor () {
+    this.first = null;
+    this.last = null;
+    this.length = 0;
+  }
+  enqueue(value) {
+    const newNode = new Node(value);
+    if (this.length === 0) {
+      this.first = newNode;
+      this.last = newNode;
+    } else {
+      this.last.next = newNode;
+      this.last = newNode;
+    }
+    this.length++;
+    return this;
+  }
+  dequeue() {
+    if (!this.first) {
+      return null;
+    }
+    if (this.first === this.last) {
+      this.last = null;
+    }
+    this.first = this.first.next;
+    this.length--;
+    return this;
+  }
+}
+
+let qu = new Queue();
+qu.enqueue("google");
+qu.dequeue();
+qu
