a614f8247be0a7c2e3fa83b2ccd948893d2ad7f6
Romaine De La Cruz <r2dcruz@gmail.com>
2020-10-27 00:36:25 -0700

    queues

--- mci-dsAlgorithms/queues.js
+++ mci-dsAlgorithms/queues.js
@@ -190,3 +190,50 @@ let qu = new Queue();
 qu.enqueue("google");
 qu.dequeue();
 qu
+
+//p r a c t i c e 
+
+class Node {
+  constructor(value) {
+    this.value = value;
+    this.next = null;
+  }
+}
+
+class Queue {
+  constructor () {
+    this.first = null;
+    this.last = null;
+    this.length = 0;
+  }
+  enqueue(value) {
+    const newNode = new Node(value);
+    if (this.length === 0) {
+      this.first = newNode;
+      this.last = newNode;
+    } else {
+      this.last.next = newNode;
+      this.last = newNode;
+    }
+    this.length++;
+    return this;
+  }
+  dequeue() {
+    const holdingPointer = this.first;
+    if (!this.first) {
+        return this;
+    }
+    if (this.first === this.last) {
+      this.last = null;
+    }
+    this.first = this.first.next;
+    this.length--;
+    return this;
+  }
+}
+
+let qu = new Queue();
+qu.enqueue("google");
+qu.enqueue("twitter");
+qu.dequeue();
+qu.dequeue();
