543161b902e5c178240cacb90066be737ec0168c
Romaine De La Cruz <r2dcruz@gmail.com>
2020-10-28 20:10:26 -0700

    nice roll

--- mci-dsAlgorithms/stacks.js
+++ mci-dsAlgorithms/stacks.js
@@ -182,3 +182,50 @@ st.push("google");
 st.push("twitter");
 st.pop();
 st.pop();
+
+class Node {
+  constructor(value) {
+    this.value = value;
+    this.next = null;
+  }
+}
+
+class Stack {
+  constructor() {
+    this.top = null;
+    this.bottom = null;
+    this.length = 0;
+  }
+
+  push(value){
+    const newNode = new Node(value)
+    if (this.length === 0) {
+      this.top = newNode;
+      this.bottom = newNode;
+    } else {
+      const holdingPointer = this.top
+      this.top = newNode;
+      this.top.next = holdingPointer;
+    }
+    this.length++;
+      return this;
+    }
+    pop(){
+      if (!this.top) {
+        return null;
+      }
+      if (this.top === this.bottom) {
+        return this.bottom = null;
+      }
+      const holdingPointer = this.top;
+      this.top = this.top.next;
+      this.length--;
+      return this;
+    }
+  }
+
+
+let st = new Stack();
+st.push("google");
+st.push("twitter");
+st.pop()
