a0ed22608f36a074a4df596b2d540229a367b281
Romaine De La Cruz <r2dcruz@gmail.com>
2020-11-06 18:41:45 -0800

    always good to do something

--- mci-dsAlgorithms/stacks.js
+++ mci-dsAlgorithms/stacks.js
@@ -229,3 +229,50 @@ let st = new Stack();
 st.push("google");
 st.push("twitter");
 st.pop()
+
+// calc and coding practice
+
+class Node {
+  constructor(value) {
+    this.value = value;
+    this.next = null;
+  }
+}
+
+class Stack {
+  constructor() {
+    this.top = null;
+    this.bottom = null;
+    this.length = 0
+  }
+  push(value) {
+    const newNode = new Node(value);
+    if (this.length === 0) {
+      this.top = newNode;
+      this.bottom = newNode;
+    } else {
+      const holdingPointer = this.top;
+      this.top = newNode;
+      this.top.next = holdingPointer;
+    }
+    this.length++;
+    return this;
+  }
+  pop() {
+    if (!this.top) {
+      return null;
+    }
+    if (this.top === this.bottom) {
+      this.bottom = null;
+    }
+    const holdingPointer = this.top;
+    this.top = this.top.next;
+    this.length--
+    return this;
+  }
+}
+
+let st = new Stack();
+st.push("google");
+st.push("hello");
+st.pop();
