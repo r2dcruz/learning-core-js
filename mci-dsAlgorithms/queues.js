dc7bf75ceca13782df130ec1c756b29ba0371ffb
Romaine De La Cruz <r2dcruz@gmail.com>
2020-11-13 21:27:47 -0800

    typing test

--- mci-dsAlgorithms/queues.js
+++ mci-dsAlgorithms/queues.js
@@ -237,3 +237,49 @@ qu.enqueue("google");
 qu.enqueue("twitter");
 qu.dequeue();
 qu.dequeue();
+
+// got this in the bag
+
+class Node {
+  constructor(value) {
+    this.value = value;
+    this.next = null;
+  }
+}
+
+class Queue {
+  constructor() {
+    this.first = null;
+    this.last = null;
+    this.length = 0;
+  }
+  enqueue(value) {
+    const newNode = new Node(value);
+    if (this.length === 0) {
+      this.first = newNode;
+      this.last = newNode;
+    } else {
+      const holdingPointer = this.first;
+      this.last = newNode;
+      this.last.next = holdingPointer;
+    }
+    this.length++;
+    return this;
+  }
+  dequeue() {
+    if (!this.first) {
+      return null;
+    }
+    if (this.first = this.bottom) {
+      this.last = null;
+    }
+    const holdingPointer = this.first;
+    this.first = this.first.next;
+    this.length--;
+    return this;
+  }
+}
+
+let qu = new Queue();
+qu.enqueue("google");
+qu.enqueue("twitter");
