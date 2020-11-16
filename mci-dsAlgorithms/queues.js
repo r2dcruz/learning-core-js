9a8c1d6d68810f1c29ce9b15df906c627748f8ef
Romaine De La Cruz <r2dcruz@gmail.com>
2020-11-15 18:34:28 -0800

    need to continue practicing

--- mci-dsAlgorithms/queues.js
+++ mci-dsAlgorithms/queues.js
@@ -97,148 +97,7 @@ class Queue {
     return this.first;
   }
 }
-
-//practice queues. really easy, trying not to forget this 
-
-class Node {
-  constructor(value) {
-    this.value = value;
-    this.next = null;
-  }
-}
-
-class Queue {
-  constructor(value) {
-    this.first = null;
-    this.last = null;
-    this.length = 0;
-  }
-  enqueue(value) {
-    const newNode = new Node(value);
-    if (this.length === 0) {
-      this.first = newNode;
-      this.last = newNode;
-    } else {
-      this.last.next = newNode;
-      this.last = newNode;
-    }
-    this.length++;
-    return this;
-  }
-  dequeue() {
-    if (!this.first) {
-      return null;
-    }
-    if (this.first === this.last) {
-      this.last = null;
-    } 
-    this.first = this.first.next;
-    this.length--;
-    return this;
-  }
-  peek() {
-    return this.first;
-  }
-}
-
-let qu = new Queue();
-qu.enqueue("google");
-qu.enqueue("twitter");
-qu.dequeue();
-
-// get a rythem going and keep practicing 
-
-class Node {
-  constructor (value) {
-    this.value = value;
-    this.next = null;
-  }
-}
-
-class Queue {
-  constructor () {
-    this.first = null;
-    this.last = null;
-    this.length = 0;
-  }
-  enqueue(value) {
-    const newNode = new Node(value);
-    if (this.length === 0) {
-      this.first = newNode;
-      this.last = newNode;
-    } else {
-      this.last.next = newNode;
-      this.last = newNode;
-    }
-    this.length++;
-    return this;
-  }
-  dequeue() {
-    if (!this.first) {
-      return null;
-    }
-    if (this.first === this.last) {
-      this.last = null;
-    }
-    this.first = this.first.next;
-    this.length--;
-    return this;
-  }
-}
-
-let qu = new Queue();
-qu.enqueue("google");
-qu.dequeue();
-qu
-
-//p r a c t i c e 
-
-class Node {
-  constructor(value) {
-    this.value = value;
-    this.next = null;
-  }
-}
-
-class Queue {
-  constructor () {
-    this.first = null;
-    this.last = null;
-    this.length = 0;
-  }
-  enqueue(value) {
-    const newNode = new Node(value);
-    if (this.length === 0) {
-      this.first = newNode;
-      this.last = newNode;
-    } else {
-      this.last.next = newNode;
-      this.last = newNode;
-    }
-    this.length++;
-    return this;
-  }
-  dequeue() {
-    const holdingPointer = this.first;
-    if (!this.first) {
-        return this;
-    }
-    if (this.first === this.last) {
-      this.last = null;
-    }
-    this.first = this.first.next;
-    this.length--;
-    return this;
-  }
-}
-
-let qu = new Queue();
-qu.enqueue("google");
-qu.enqueue("twitter");
-qu.dequeue();
-qu.dequeue();
-
-// got this in the bag
+// cleaning out got this structure mixed up with stacks
 
 class Node {
   constructor(value) {
@@ -259,27 +118,15 @@ class Queue {
       this.first = newNode;
       this.last = newNode;
     } else {
-      const holdingPointer = this.first;
+      this.last.next = newNode;
       this.last = newNode;
-      this.last.next = holdingPointer;
     }
     this.length++;
-    return this;
-  }
-  dequeue() {
-    if (!this.first) {
-      return null;
-    }
-    if (this.first = this.bottom) {
-      this.last = null;
-    }
-    const holdingPointer = this.first;
-    this.first = this.first.next;
-    this.length--;
     return this;
   }
 }
 
 let qu = new Queue();
 qu.enqueue("google");
-qu.enqueue("twitter");
+qu.enqueue("twitter")
+qu.enqueue("amazon")
