24cb977a5eeb1b526d89ce12b130f4feef3d9973
Romaine De La Cruz <r2dcruz@gmail.com>
2020-11-16 22:30:05 -0800

    happy day

--- mci-dsAlgorithms/queues.js
+++ mci-dsAlgorithms/queues.js
@@ -130,3 +130,36 @@ let qu = new Queue();
 qu.enqueue("google");
 qu.enqueue("twitter")
 qu.enqueue("amazon")
+
+//
+class Node {
+  constructor(value) {
+    this.value = value;
+    this.next = null;
+  }
+}
+
+class Queue {
+  constructor() {
+    this.first = null;
+    this.last = null;
+    this.length = 0;
+  }
+  enqueue(value) {
+    const newNode = new Node(value);
+    if (this.length === 0) {
+      this.first = newNode;
+      this.last = newNode;
+    } else {
+      this.last.next = newNode;
+      this.last = newNode;
+    }
+    this.length++;
+    return this;
+  }
+}
+
+let qu = new Queue();
+qu.enqueue("google");
+qu.enqueue("twitter")
+qu.enqueue("amazon")
