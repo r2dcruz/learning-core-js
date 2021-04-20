8620f19343189b40ff69a4e4d99fe0b643d3f227
Romaine De La Cruz <r2dcruz@gmail.com>
2021-04-19 22:05:19 -0700

    hello again

--- mci-dsAlgorithms/linkedList.js
+++ mci-dsAlgorithms/linkedList.js
@@ -1085,3 +1085,28 @@ class LinkedList {
 
 let ll = new LinkedList("google");
 ll
+
+// start of practice again.....
+
+class Node {
+  constructor(value) {
+    this.value = value;
+    this.next = null;
+  }
+}
+
+class LinkedList {
+  constructor(value) {
+    this.head = {
+        value: value,
+        next: null
+    };
+    this.tail = this.head;
+    this.length = 1;
+  }
+}
+
+let ll = new LinkedList("hello");
+ll
+
+//still got it!
